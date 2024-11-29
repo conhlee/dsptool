b64634570aa56a0419a6c384dcfe82d3f05a808d
Angelo <48489278+conhlee@users.noreply.github.com>
2024-11-29 17:54:27 +0100

    Fixes

--- main.c
+++ main.c
@@ -16,53 +16,46 @@ int main(int argc, char** argv) {
     if (argc < 3)
         usage();
 
-    const int outputSpecified = argc >= 4;
+    const int outputSpecified = argc >= 3;
 
-    const char* mode = argv[1];
-    const char* inputPath = argv[2];
+    //const char* mode = argv[1];
+    const char* inputPath = argv[1];
 
     char* outputPath = NULL;
 
     FileHandle inputHndl = { 0 };
     FileHandle outputHndl = { 0 };
 
-    if (strcasecmp(mode, "towav") == 0) {
-        outputPath = outputSpecified ? argv[3] : strcat(strdup(inputPath), ".wav");
+    outputPath = outputSpecified ? argv[2] : strcat(strdup(inputPath), ".wav");
 
-        inputHndl = FileCreateHandle(inputPath);
+    inputHndl = FileCreateHandle(inputPath);
 
-        printf("dsptool > towav: exporting to \"%s/\"..\n", outputPath);
+    printf("dsptool > towav: exporting to \"%s/\"..\n", outputPath);
 
-        printf("Decoding DSP data..");
+    printf("Decoding DSP data..");
 
-        DspPreprocess(inputHndl.data_u8);
+    DspPreprocess(inputHndl.data_u8);
 
-        s16* samples = DspDecodeSamples(inputHndl.data_u8);
-        u32 sampleCount = DspGetSampleCount(inputHndl.data_u8);
-        u32 sampleRate = DspGetSampleRate(inputHndl.data_u8);
+    s16* samples = DspDecodeSamples(inputHndl.data_u8);
+    u32 sampleCount = DspGetSampleCount(inputHndl.data_u8);
+    u32 sampleRate = DspGetSampleRate(inputHndl.data_u8);
 
-        LOG_OK;
+    LOG_OK;
 
-        FileDestroyHandle(inputHndl);
+    FileDestroyHandle(inputHndl);
 
-        printf("    - Decoded %u samples. (sampleRate=%u)\n", sampleCount, sampleRate);
+    printf("    - Decoded %u samples. (sampleRate=%u)\n", sampleCount, sampleRate);
 
-        printf("Writing WAV to \"%s\"..", outputPath);
+    printf("Writing WAV to \"%s\"..", outputPath);
 
-        outputHndl = WavBuild(samples, sampleCount, sampleRate, 1);
+    outputHndl = WavBuild(samples, sampleCount, sampleRate, 1);
 
-        FileWriteHandle(outputHndl, outputPath);
+    free(samples);
 
-        LOG_OK;
-    }
-    else if (strcasecmp(mode, "todsp") == 0) {
-        //outputPath = outputSpecified ? argv[3] : strcat(strdup(inputPath), ".dsp");
+    FileWriteHandle(outputHndl, outputPath);
+    FileDestroyHandle(outputHndl);
 
-        // TODO
-        panic("WAV > DSP is unimplemented");
-    }
-    else
-        usage();
+    LOG_OK;
     
     if (!outputSpecified)
         free(outputPath);
